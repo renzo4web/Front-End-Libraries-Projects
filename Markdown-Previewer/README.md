5cd18f8ccfb40ce519690e193cd057a7bfa98660
w3renzo <w3renzo@gmail.com>
2020-12-17 02:05:27 -0300

    Project Ended

---
+++ Markdown-Previewer/README.md
@@ -0,0 +1,50 @@
+# Markdown Previewer
+
+> A simple markdown previewer
+> Part of the [Introduction to the Front End Libraries Projects](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer) from FreeCodeCamp
+
+![screenshot](https://i.imgur.com/DfC8gve.png)
+
+- Generate random quotes.
+- click quote to copy to clipboard.
+- Button to tweet the quote chosed directly.
+
+## Built With
+
+- Javascript Vanilla
+- CSS
+- HTML
+
+## Live Demo
+
+[Live Demo Link](https://codepen.io/w3renzo/pen/MWjmVoj)
+
+## What I learned?
+
+**In this project i learned to use a library in this case i used a markdown library**
+
+## Authors
+
+👤 **Author**
+
+- GitHub: [@w3renzo](https://github.com/w3renzo)
+- Twitter: [@w3renzo](https://twitter.com/w3renzo)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Thanks to [Marked Lybrary](https://github.com/markedjs/marked?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library)
+- Thanks to [FreeCodeCamp](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine) for the idea.
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
--- Markdown-Previewer/app/app.js
+++ Markdown-Previewer/app/app.js
@@ -1,3 +1,48 @@
+const markdownExample =
+  '# Welcome to my React Markdown Previewer!\n' +
+  '\n' +
+  '## This is a sub-heading...\n' +
+  "### And here's some other cool stuff:\n" +
+  '\n' +
+  'Heres some code, `<div></div>`, between 2 backticks.\n' +
+  '\n' +
+  '```' +
+  '// this is multi-line code:\n' +
+  '\n' +
+  'function anotherExample(firstLine, lastLine) {\n' +
+  "  if (firstLine == '```' && lastLine == '```') {\n" +
+  '    return multiLineCode;\n' +
+  '  }' +
+  '}\n' +
+  '```\n' +
+  '\n' +
+  'You can also make text **bold**... whoa!\n' +
+  'Or _italic_.\n' +
+  'Or... wait for it... **_both!_**\n' +
+  'And feel free to go crazy ~~crossing stuff out~~.\n' +
+  '\n' +
+  "There's also [links](https://www.freecodecamp.com), and\n" +
+  '> Block Quotes!\n' +
+  '\n' +
+  'And if you want to get really crazy, even tables:\n' +
+  '\n' +
+  'Wild Header | Crazy Header | Another Header?\n' +
+  '------------ | ------------- | -------------\n' +
+  'Your content can | be here, and it | can be here....\n' +
+  'And here. | Okay. | I think we get it.\n' +
+  '\n' +
+  '- And of course there are lists.\n' +
+  '  - Some are bulleted.\n' +
+  '     - With different indentation levels.\n' +
+  '        - That look like this.\n' +
+  '\n' +
+  '\n' +
+  '1. And there are numbererd lists too.\n' +
+  '1. Use just 1s if you want!\n' +
+  "1. And last but not least, let's not forget embedded images:\n" +
+  '\n' +
+  '![React Logo w/ Text](https://goo.gl/Umyytc)\n';
+
 const textArea = document.getElementById('editor');
 const preview = document.getElementById('preview');
 marked.setOptions({
@@ -20,6 +65,9 @@ marked.setOptions({
   xhtml: false,
 });
 marked.Renderer();
+
+textArea.textContent = markdownExample;
+preview.innerHTML = marked(markdownExample);
 t = textArea.addEventListener('input', () => {
   let g = textArea.value;
   //let r = g.split('');
@@ -27,4 +75,3 @@ t = textArea.addEventListener('input', () => {
   preview.innerHTML = marked(g);
   console.log(marked(g));
 });
-//console.log(marked('# heading+'));
